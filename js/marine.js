bfd2e460eee84194916054cf7150b3e0e6f23716
Marine Belin <marinebelin.39@gmail.com>
2017-04-25 16:02:07 +0200

    javascript : entrer dans le btn start ok

---
+++ js/marine.js
@@ -0,0 +1,13 @@
+// Déclaration des variables
+	var btnStart = document.querySelector('.start button');
+	var startScreen = document.querySelector('.start');
+
+// Ecouteur d'évenements
+	btnStart.addEventListener('click',startClick);
+
+
+// FONCTIONS
+	function startClick(){
+		console.log("J'ai pu cliqué !!");
+		
+	}
