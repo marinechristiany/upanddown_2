4198768b091e01ab0156d6fe627dafaa03b0420d
Claire Giacomel <claire.giacomel@gmail.com>
2017-04-25 17:00:52 +0200

    dernières modifs du soir

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ var ecranStart=document.querySelectorAll('.start');
 var btn=document.querySelector('.start button');
 var ecranChoose=document.querySelector('.choose');
 var ecranJeu=document.querySelector('section');
+var titre=document.querySelector('h2');
+
+
 
 //DETECTEURS D'EVENEMENTS
 btn.addEventListener('click', goEcranChoose);
@@ -13,10 +16,9 @@ function goEcranChoose(){
     for (i=0; i<ecranStart.length; i++){
         ecranStart[i].classList.remove('start');
         ecranStart[i].classList.add('hidden');
-        ecranChoose.classList.remove('hidden');
-        ecranJeu.style.marginTop = "213px";
-    
     }
+    ecranChoose.classList.remove('hidden');
+    titre.classList.add('start-opa');
 }
 
 
