9d50b4f58d2bf7ad57ccf27f792ef219ee9894a0
Claire Giacomel <claire.giacomel@gmail.com>
2017-04-25 15:53:56 +0200

    modifs css !

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 // VARIABLES
 var ecranStart=document.querySelectorAll('.start');
 var btn=document.querySelector('.start button');
-var ecranChoose=document.querySelector('.choose')
+var ecranChoose=document.querySelector('.choose');
+var ecranJeu=document.querySelector('section');
 
 //DETECTEURS D'EVENEMENTS
 btn.addEventListener('click', goEcranChoose);
@@ -10,7 +11,10 @@ btn.addEventListener('click', goEcranChoose);
 //FONCTION
 function goEcranChoose(){
     for (i=0; i<ecranStart.length; i++){
-        ecranStart[i].classList.add('.hidden')
+        ecranStart[i].classList.remove('start');
+        ecranStart[i].classList.add('hidden');
+        ecranChoose.classList.remove('hidden');
+        ecranJeu.style.marginTop = "213px";
     
     }
 }
