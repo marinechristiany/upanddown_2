b2b1cbee29561eb0572b9ce995979402cad53304
samuelnicolas <mangaprelude@gmail.com>
2017-04-26 11:17:50 +0200

    jsactualise

--- js/scriptsam.js
+++ js/scriptsam.js
@@ -1,5 +1,8 @@
 //déclaration des variables
  var buttonstart = document.querySelector ('.start button');
+var start = document.querySelectorAll ('.start');
+var title = document.querySelector ('h2');
+var choose = document.querySelector ('.choose');
 
 //ajouter observateur évenements
 buttonstart.addEventListener("click",changeecran)
@@ -10,5 +13,14 @@ buttonstart.addEventListener("click",changeecran)
 //fonctions
 function changeecran()
 {
-alert("Vous venez de cliquer sur le bouton!")
+for (i=0; i<start.length ;i++){
+    start[i].classList.remove('start')
+    start[i].classList.add('hidden')
+    console.log(i);
 }
+    title.classList.add('start-opa');
+    choose.classList.add('choose');
+    choose.classList.remove('hidden');
+    
+}
+
