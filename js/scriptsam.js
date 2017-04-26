591b229a81ee7dd06dfb1fb24a52c91cb095835a
samuelnicolas <mangaprelude@gmail.com>
2017-04-26 09:54:03 +0200

    yesterday

---
+++ js/scriptsam.js
@@ -0,0 +1,14 @@
+//déclaration des variables
+ var buttonstart = document.querySelector ('.start button');
+
+//ajouter observateur évenements
+buttonstart.addEventListener("click",changeecran)
+
+
+
+
+//fonctions
+function changeecran()
+{
+alert("Vous venez de cliquer sur le bouton!")
+}
