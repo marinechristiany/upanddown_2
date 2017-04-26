cef895f3c13a356a9b241330934ccf798fbc1d56
samuelnicolas <mangaprelude@gmail.com>
2017-04-26 13:14:23 +0200

    corrige

--- js/scriptsam.js
+++ js/scriptsam.js
@@ -1,26 +1,29 @@
 //déclaration des variables
- var buttonstart = document.querySelector ('.start button');
+var buttonstart = document.querySelector('.start button');
 var start = document.querySelectorAll ('.start');
 var title = document.querySelector ('h2');
 var choose = document.querySelector ('.choose');
+var aleatoire = Math.floor(Math.random() * 101);
+var utilisateur = parseInt(document.querySelector('input').value);
+console.log(aleatoire);
 
 //ajouter observateur évenements
-buttonstart.addEventListener("click",changeecran)
+buttonstart.addEventListener("click",changeecran);
 
 
 
 
 //fonctions
-function changeecran()
-{
-for (i=0; i<start.length ;i++){
-    start[i].classList.remove('start')
-    start[i].classList.add('hidden')
-    console.log(i);
-}
+function changeecran(){
+
+    for (i=0; i<start.length ;i++){
+        start[i].classList.remove('start');
+        start[i].classList.add('hidden');
+    }
     title.classList.add('start-opa');
     choose.classList.add('choose');
     choose.classList.remove('hidden');
     
 }
+
 
